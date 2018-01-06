889a092e4200047d2fcfe17a48508f9c6a4d9dd9
Choyon Ahmed <choyonbd.tk@gmail.com>
2018-01-06 13:38:33 +0600

    Update a.php

--- a.php
+++ a.php
@@ -1,7 +1,7 @@
 <? 
   echo('Current working directory on the server: <br><br>');
   echo('<textarea rows="1" cols="100">');
-  echo getcwd();
+  echo __DIR__;
   echo('</textarea>');
   echo('<br><br><h2>Instructions:</h2><br><b>Copy this above directory address with appropriate slashes till the folder name that you see in your address bar!
   <br>For example, let you are in abc.com/img/usr/a.php and the working directory is /home/user/abc.com/img/usr/<br>
